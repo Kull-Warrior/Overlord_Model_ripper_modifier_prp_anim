d5b00f6ef1bba0c8bf064dd5698a82c74c9c4b86
Kull-Warrior <tobias992000@gmail.com>
2024-05-03 20:38:45 +0200

    Moved libraries one directory up
    The libraries were unnecessarily stored two sub folder deep

--- newGameLib/__init__.py
+++ newGameLib/__init__.py
@@ -1,2 +1,20 @@
-import myLibraries
-from myLibraries import *
+import myFunction
+import binaresLib
+import imageLib
+import meshLib
+import actionLib
+import skeletonLib
+
+reload(myFunction)
+reload(binaresLib)
+reload(imageLib)
+reload(meshLib)
+reload(actionLib)
+reload(skeletonLib)
+
+from myFunction import *
+from binaresLib import *
+from imageLib import *
+from meshLib import *
+from actionLib import *
+from skeletonLib import *
